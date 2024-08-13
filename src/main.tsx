10d7cbe2de33ba3d7eeb236a3122f2769fec785f
enkuria <kengeene@gmail.com>
2024-08-13 20:56:24 +0300

    feat: add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@/assets/css/index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <div>About</div>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
