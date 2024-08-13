c142f0c165e9637f9593d665a063f9f43bed6a81
enkuria <kengeene@gmail.com>
2024-08-13 14:57:30 +0300

    refactor: add forex bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import TopLinks from "@/components/topLinks";
 import Banner from "@/assets/img/banner.png";
 import PersonalSolutions from "@/components/personal-solutions";
 import CorporateSolutions from "@/components/corporate-solutions";
+import ForexBar from "@/components/forex-bar";
+
 function App() {
   return (
     <div className="w-screen">
@@ -9,11 +11,14 @@ function App() {
       <div>
         <img src={Banner} width="100%" />
       </div>
-      <div className="text-center mx-10 py-10">
+      <div className="text-center py-10">
         <PersonalSolutions />
       </div>
-      <div className="text-center mx-10 py-10 bg-[#F5F0F0]">
+      <div className="text-center py-10 bg-[#F5F0F0]">
         <CorporateSolutions />
+      </div>
+      <div className="flex flex-row justify-evenly py-3 bg-[#F9EBE8]">
+        <ForexBar />
       </div>
     </div>
   );
---
+++ src/components/forex-bar.tsx
@@ -0,0 +1,22 @@
+export default function ForexBar() {
+  return (
+    <>
+      <span className="text-center">FOREX</span>
+      <span>
+        <strong>USD:</strong> Buying: 86.17
+      </span>
+      <span>
+        <strong>GDP:</strong> Buying: 134.55
+      </span>
+      <span>
+        <strong>EURO:</strong> Buying: 136.17
+      </span>
+      <span>
+        <strong>Indian Rupee:</strong> Buying: 1.2552
+      </span>
+      <span>
+        <strong>Danish Kroner:</strong> Buying: 13.4965
+      </span>
+    </>
+  );
+}
