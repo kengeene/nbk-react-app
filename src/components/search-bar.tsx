96fa61766a5a9a3d04a101d0f7444edd41f5844f
enkuria <kengeene@gmail.com>
2024-08-14 21:02:16 +0300

    refaactor: use exported search icon

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,15 +1,15 @@
-import { Search } from "lucide-react";
-import { Input } from "@/components/shared/input";
+// import { Search } from "lucide-react";
+import SearchIcon from "@/assets/img/search-icon.svg";
 
 export default function SearchBar() {
   return (
-    <div className="w-1/4 relative flex-1 md:grow-0">
-      <Input
+    <div className="w-2/3 flex flex-row md:grow-0 border border-[#30120B rounded-md px-3 py-2">
+      <input
         type="search"
         placeholder="I am looking for..."
-        className="w-full rounded-lg bg-background md:w-[200px] lg:w-[336px]"
+        className="w-full rounded-lg bg-background border-none my-3.5"
       />
-      <Search className="absolute right-1.5 top-2.5 h-5 w-5 bg-[#30120B] text-[#FFAA00] rounded-sm" />
+      <img src={SearchIcon} />
     </div>
   );
 }
